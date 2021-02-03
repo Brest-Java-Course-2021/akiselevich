007c69572f01ca3de0bc558c319b701b75ecec03
Kiselevich-Alex <54080508+Kiselevich-Alex@users.noreply.github.com>
2021-02-03 17:56:07 +0300

    added new sub repo

---
+++ price-calc/src/main/java/com/epam/brest/Main.java
@@ -0,0 +1,11 @@
+package main.java.com.epam.brest;
+
+public class Main {
+    public static void main(String[] args) {
+        double a = 3.0;
+        double b = 4.0;
+        double c = 5.0;
+
+        System.out.println("perimeter: " + (a + b + c));
+    }
+}
